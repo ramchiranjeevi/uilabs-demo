bcf08fc5f7b7345a6c826fff8d9398b2c10af55a
SowmyaT <tsowmya512@gmail.com>
2016-02-25 13:29:04 +0530

    indented menu component related files

--- app/components/menu-component.js
+++ app/components/menu-component.js
@@ -4,14 +4,11 @@ export default Ember.Component.extend({
 
   subMenuObj : [],
 
-
   mouseMove( event ){
-    var self = this;
     event.stopPropagation();
     var subMenus = this.get('menuItem.children');
     this.set('subMenuObj', subMenus);
-    if(subMenus.length)
-    {
+    if(subMenus.length){
       self.$(".ui.dropdown.item").addClass("active visible");
       self.$(".ui.dropdown.item .menu.transition" ).removeClass("hidden");
       self.$(".ui.dropdown.item .menu.transition").addClass("visible");
