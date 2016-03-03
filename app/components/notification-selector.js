2b0927dfb5702e4c507925bd7e9c4956f6302c41
Renukutty <renugadevi.p@zohocorp.com>
2016-03-03 10:33:24 +0530

    classNamebinding removed

--- app/components/notification-selector.js
+++ app/components/notification-selector.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
     tagName: 'div',
-    classNameBindings: [ui],
 
     didInsertElement:function() {
         console.log('its come');
