d834349f4a26aea7a831b4dfa292f4900ce810ae
Priyanga Vanangamudi <gayathripriya.sb@gmail.com>
2016-03-08 16:55:15 +0530

    name empty message added

--- app/routes/closer-actions.js
+++ app/routes/closer-actions.js
@@ -2,6 +2,23 @@ import Ember from 'ember';
 
 const { Route, Object, Logger, inject } = Ember;
 
+let CreateFolderModelClass = Object.extend({
+  name: null,
+
+  save() {
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      // console.log((Math.random() < 0.26));
+      if (Math.random() < 0.26 ) {
+        Logger.error('server error');
+        reject('Name should not be empty, please try again');
+      } else {
+        Logger.info('saved model');
+        resolve();
+      }
+    });
+  }
+});
+
 export default Route.extend({
     modelObject: inject.service("create-folder-model"),
     model() {
