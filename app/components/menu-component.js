1cef1878d3fee551e5423b8436dcb572f619472c
SowmyaT <tsowmya512@gmail.com>
2016-03-09 12:02:10 +0530

    recursive calling menus component

--- app/components/menu-component.js
+++ app/components/menu-component.js
@@ -4,15 +4,52 @@ import MenuMixin from '../mixins/menumixin';
 export default Ember.Component.extend( MenuMixin, {
 
   menuObj: [],
+  tabObj: [],
   subMenuObj : [],
   isShowSubMenu: false,
+  temp_index: 1,
+
+   //RECURSIVE CALLING FOR MENUNODE
+
+   displayedChildren: function() {
+     var self =  this;
+    return this.get("menuNode").filter(function(node) {
+      return node.children;
+    });
+  }.property("node.children"),
+
+   didInsertElement(){
+     var self = this;
+     $ = Ember.$;
+     self.$("#createNew").attr({ tabindex: 1}), self.$("#createNew").focus();
+     var menuObj = self.get('menuNode');
+     self.set('menuObj', menuObj);
+     var children = [];
+       menuObj.forEach(function (node){
+         children = node.children;
+         if(children.length !== 0)
+          {
+             self.get('subMenuObj').addObject(Ember.Object.create(children)); // No I18N
+           }
+        }, this);
+   },
+
+   //KeyDown EVENT ACTION HANDLER
+
+   keyDown(event){
+     var self = this;
+     var targetId = event.target.id,
+     temp_model = self.get('menuObj'), // No I18N
+     temp_index = self.get('temp_index');  // No I18N
+     console.log(temp_model);
+   },
 
   //CLICK EVENT ACTION HANDLER
 
   click(event){
     var self = this;
     event.stopPropagation();
-    var menuAction = self.get('menuItem.action');
+    var menuAction = self.get('node.action');
     if(menuAction !== "")
     {
       self.send(menuAction);
@@ -24,37 +61,18 @@ export default Ember.Component.extend( MenuMixin, {
   mouseMove( event ){
     event.stopPropagation();
     var self = this,
-    controller = self._controller,
     $ = Ember.$,
-    targetParentEle = event.target.parentElement,
-    parentDiv = targetParentEle.parentNode.id,
-    subMenus = self.get('menuItem.children'),
-    isSubMenu = self.get('menuItem.isSubMenu'),
-    isMoreMenu = controller.get('isMoreMenu'),
-    isContextMenu = controller.get('isContextMenu'),
-    submenudiv = $("#submenudiv");
-    self.set('subMenuObj', subMenus);
-    if(subMenus.length)
+    targetId = event.target.id;
+    if(targetId === "more" || targetId === "submenudiv")
     {
       self.set('isShowSubMenu', true);
-      submenudiv.css({'display': 'block'});
-      if(isMoreMenu || isContextMenu)
-      {
-          if(parentDiv === "basicmenudiv")
-          {
-            controller.set('isMoreMenu', false);
-            controller.set('isContextMenu', false);
-          }
-      }
+      $("#submenudiv").css({'display': 'block'});
     }
     else {
       self.set('isShowSubMenu', false);
-      submenudiv.css({'display': 'none'});
-      if(isSubMenu)
-      {
-        self.set('isShowSubMenu', true);
-        submenudiv.css({'position': 'absolute', 'display': 'block'});
-      }
+      $("#submenudiv").css({'display': 'none'});
     }
   },
+
+
 });
--- app/routes/menu.js
+++ app/routes/menu.js
@@ -2,19 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var name = [{'action': 'createfolderAction', 'name':'Create New', 'children': [], 'icon' : 'edit icon'},
-    {'action': 'openAction', 'name':'Open', 'children': [], 'icon' : 'globe icon'},
-    {'action': 'shareAction', 'name':'Share', 'children': [], 'icon' : 'settings icon'},
-    {'action': 'cutAction', 'name':'Cut', 'children': [], 'icon' : 'edit icon', 'type': '⌘X'},
-    {'action': 'copyAction', 'name':'Copy', 'children': [],  'icon' : 'edit icon', 'type': '⌘C'},
-    {'action': 'pasteAction', 'name':'Paste', 'children': [], 'icon' : 'globe icon', 'type': '⌘V'},
-    {'action': 'renameAction', 'name':'Rename', 'children': [],'icon' : 'edit icon'},
-    {'action': 'duplicateAction', 'name':'Duplicate', 'children': [], 'icon' : 'settings icon'},
-    {'action': 'downloadAction', 'name':'Download', 'children': [], 'icon': 'globe icon' },
-    {'action': '', 'name': 'More', 'arrowClass':'dropdown', 'icon' : 'grid layout icon',
-    'children': [{'action':'readAction', 'name': 'Read Only' , 'isSubMenu': 'true', 'icon' : 'edit icon', 'arrowClass':'', 'type': '.ro', 'children': []},
-    {'action':'readWriteAction', 'name': 'Read Write' ,  'isSubMenu': 'true', 'icon' : 'globe icon', 'arrowClass':'', 'type': '.rw', 'children': []},
-    {'action':'coOwnerAction', 'name': 'Co Owner' , 'isSubMenu': 'true', 'icon' : 'settings icon', 'arrowClass':'', 'type': '.co','children': []},]}];
+    var name = [{'action': 'createfolderAction', 'name':'Create New', 'id': 'createNew', 'children': [], 'icon' : 'edit icon', 'tab_index' : 0 },
+    {'action': 'openAction', 'name':'Open', 'id': 'open', 'children': [], 'icon' : 'globe icon', 'tab_index' : 1 },
+    {'action': 'shareAction', 'name':'Share', 'id': 'share', 'children': [], 'icon' : 'settings icon', 'tab_index' : 2 },
+    {'action': 'cutAction', 'name':'Cut', 'id': 'cut', 'children': [], 'icon' : 'edit icon', 'type': '⌘X', 'tab_index' : 3},
+    {'action': 'copyAction', 'name':'Copy', 'id': 'copy', 'children': [],  'icon' : 'edit icon', 'type': '⌘C', 'tab_index' : 4},
+    {'action': 'pasteAction', 'name':'Paste', 'id': 'paste', 'children': [], 'icon' : 'globe icon', 'type': '⌘V', 'tab_index' : 5 },
+    {'action': 'renameAction', 'name':'Rename', 'id': 'rename', 'children': [],'icon' : 'edit icon', 'tab_index' : 6 },
+    {'action': 'duplicateAction', 'name':'Duplicate', 'id': 'duplicate', 'children': [], 'icon' : 'settings icon', 'tab_index' : 7},
+    {'action': 'downloadAction', 'name':'Download', 'id': 'download', 'children': [], 'icon': 'globe icon', 'tab_index' : 8 },
+    {'action': '', 'name': 'More',  'id': 'more', 'arrowClass':'dropdown', 'icon' : 'grid layout icon', 'tab_index' : 9,
+    'children': [{'action':'readAction', 'name': 'Read Only' , 'isSubMenu': 'true', 'icon' : 'edit icon', 'arrowClass':'', 'tab_index' : 0, 'type': '.ro', 'children': []},
+    {'action':'readWriteAction', 'name': 'Read Write' ,  'isSubMenu': 'true', 'icon' : 'globe icon', 'arrowClass':'', 'tab_index' : 1, 'type': '.rw', 'children': []},
+    {'action':'coOwnerAction', 'name': 'Co Owner' , 'isSubMenu': 'true', 'icon' : 'settings icon', 'arrowClass':'', 'tab_index' : 2, 'type': '.co','children': []},]}];
     return name;
-  }
+  },
+
 });
