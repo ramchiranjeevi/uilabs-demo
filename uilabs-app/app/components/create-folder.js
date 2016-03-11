c9153d547d453cdd8cb396a3371cca052081813e
Ram Chiranjeevi <ramchiranjeevi@gmail.com>
2016-03-11 11:35:47 +0530

    Moved to uilabs-app

--- uilabs-app/app/components/create-folder.js
+++ uilabs-app/app/components/create-folder.js
@@ -5,4 +5,10 @@ export default Ember.Component.extend({
     initialization: function(){
         this.set( "requiredService", Ember.inject.service( this.get("serviceName") ) );
     }.on("didReceiveAttrs"),
+    createWS(){
+      var model = {
+        fn: 'Un ws'
+      }
+      ModelDoalgo.create('compname', 'servicename', model, dialogOptions);
+    }
 });
